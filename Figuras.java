d11ab9c8abb22d3c3ac0f64893818764b1bb3a21
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-03-29 10:38:54 -0300

    Add files via upload

---
+++ Figuras.java
@@ -0,0 +1,11 @@
+public class Figuras {
+    public static void main(String[] args) {
+                System.out.println("XXXXX");
+                System.out.println("X   X");
+                System.out.println("X   X");
+                System.out.println("X   X");
+                System.out.println("XXXXX");
+            }
+        }
+
+
